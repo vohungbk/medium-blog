703abe886451c8e6ff93e8756ecb3caf0adfff95
Hung Vo Viet <vohungbk@gmail.com>
2023-07-10 08:59:17 +0700

    Add config format code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-const newsRouter = require("./news");
-const siteRouter = require("./site");
+const newsRouter = require('./news');
+const siteRouter = require('./site');
 
 function routes(app) {
-  app.use("/news", newsRouter);
-  app.use("/", siteRouter);
+  app.use('/news', newsRouter);
+  app.use('/', siteRouter);
 }
 
 module.exports = routes;
